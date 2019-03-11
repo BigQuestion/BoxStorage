04e9afcda9ea35543c4b6dcd125b8142442fef16
BigQuestion <176562986@qq.com>
2019-03-11 14:55:51 +0800

    1

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="app" style="height: 100%;width: 100%;">
+    <BoxStorage/>
+  </div>
+</template>
+
+<script>
+import BoxStorage from './components/BoxStorage'
+export default {
+  name: 'app',
+  components: {
+    BoxStorage,
+  }
+}
+</script>
+
+<style>
+html{
+  height: 100%;
+  width: 100%;
+}
+</style>
---
+++ src/components/BoxStorage.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="hello">
+    <div v-for="item in boxData" class="box"></div>
+  </div>
+</template>
+<script>
+export default {
+  name: 'HelloWorld',
+  data() {
+    return {
+      boxData:[],
+    }
+  },
+  methods: {
+    getData(){
+      for(var a = 0; a<=9;a++){
+        this.boxData.push(a)
+      }
+    }
+  },
+  mounted() {
+    this.getData();
+  },
+}
+</script>
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.hello {
+  position: fixed;
+  display: flex;
+  flex-wrap: wrap;
+  width: 550PX;
+  height: 380px;
+  left: calc(50% - 275px);
+  top: calc(50% - 190px);
+}
+
+.box {
+  border: 2px solid rgb( 190, 240, 249);
+  background-color: rgb(57, 146, 212);
+  width: 100px;
+  height: 180px;
+  box-sizing: border-box;
+  margin: 5px;
+}
+</style>
